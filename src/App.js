e1b40aa90ea479cd56664b9b80b70cb517be4cf8
Larosh Benny <57269077+typeerror101@users.noreply.github.com>
2023-02-02 18:47:23 +0530

    Day2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import useBookSearch from './useBookSearch';
 import './App.css'; 
 
@@ -6,21 +6,31 @@ export default function App() {
   const [query, setQuery] = useState('')
   const [pageNumber, setPageNumber] = useState(1)
 
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    console.log(node)
+  })
+  
   function handleSearch(e){
     setQuery(e.target.value)
     setPageNumber(1)
   }
-  
-  useBookSearch(query, pageNumber)
+   
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+  } = useBookSearch(query, pageNumber)
+
   return (
     <>
-      <input type="text" onChange={handleSearch}/>
-      <div>Title</div>  
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Loading...</div>
-      <div>Error</div>
+      <input type="text" value={query} onChange={handleSearch}/>
+      {books.map(book => {
+          return <div key={book}>{book}</div>
+      })}
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
     </>
   );
 }
--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -2,13 +2,18 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 export default function useBookSearch(query, pageNumber) {
-    const [loading, setLoding] = useState(true)
+    const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
     const [books, setBooks] = useState([])
     const [hasMore, setHasMore] = useState(false)
 
-
     useEffect(() => {
+        setBooks([])
+    }, [query])
+
+    useEffect(() => { 
+        setLoading(true)
+        setError(false)
         let cancel
 
         axios({
@@ -17,15 +22,18 @@ export default function useBookSearch(query, pageNumber) {
             params: { q: query, page: pageNumber },
             cancelToken: new axios.CancelToken(c => cancel = c )
         }).then(res => {
-            console.log(res.data)
+            setBooks(prevBooks => {
+                return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
+            })
+            setHasMore(res.data.docs.length > 0)
+            setLoading(false)
         }).catch(e => {
             if (axios.isCancel(e)) return 
+            setError(true)
         })
 
         return () => cancel();
     }, [query, pageNumber])
 
-    return (
-        null
-    )
+    return { loading, error, books, hasMore }
 }
