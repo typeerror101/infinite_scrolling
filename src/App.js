cb1d6f426e35b2f5398d2501fa2e3efaa5aedc54
Larosh Benny <57269077+typeerror101@users.noreply.github.com>
2023-02-02 23:56:32 +0530

    final product

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ export default function App() {
   const [query, setQuery] = useState('')
   const [pageNumber, setPageNumber] = useState(1)
 
-  const observer = useRef()
-  const lastBookElementRef = useCallback(node => {
-    console.log(node)
-  })
-  
-  function handleSearch(e){
-    setQuery(e.target.value)
-    setPageNumber(1)
-  }
-   
   const {
     books,
     hasMore,
@@ -23,11 +13,32 @@ export default function App() {
     error
   } = useBookSearch(query, pageNumber)
 
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return 
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    }) 
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore]) 
+
+  function handleSearch(e){
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+   
   return (
     <>
-      <input type="text" value={query} onChange={handleSearch}/>
-      {books.map(book => {
-          return <div key={book}>{book}</div>
+      <input type="text" value={query} onChange={handleSearch} placeholder="Search Book Name"/>
+      {books.map((book, index) => {
+          if(books.length === index + 1){
+            return <div ref={lastBookElementRef} key={book}>{book}</div>
+          } else {
+            return <div key={book}>{book}</div>
+          }
       })}
       <div>{loading && 'Loading...'}</div>
       <div>{error && 'Error'}</div>
